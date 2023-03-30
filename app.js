eaa0ad1b50f451199d79bbfbc9b2e4b81a57df5b
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2023-03-29 20:37:39 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get the button elements
+const buttons = document.querySelectorAll('button');
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
